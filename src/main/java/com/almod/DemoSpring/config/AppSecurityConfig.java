0d3fdcf6464eb117a31d1a4cd4bdc91d8174a9c6
Mordyasov Alex <mordyasov.60@mail.ru>
2020-09-22 17:14:39 +0700

    fix the problem with bean PasswordEncoder

--- src/main/java/com/almod/DemoSpring/config/AppSecurityConfig.java
+++ src/main/java/com/almod/DemoSpring/config/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,6 +30,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Lazy
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder(8);
     }
